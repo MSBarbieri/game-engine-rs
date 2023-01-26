5afa0590d938a4c4323d01c342eaa4118eafc0e0
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-01-26 06:56:31 -0300

    normal base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+    debug!("hello debug!");
+    Ok(())
 }
