866bde28e276355ec2e364f3a2ed0847cc1bcc7b
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-02-02 07:01:35 -0300

    sub modules init

---
+++ desktop/src/main.rs
@@ -0,0 +1,60 @@
+#[macro_use]
+extern crate log;
+
+use core::vertex::Vertex;
+
+use anyhow::Result;
+use winit::{
+    event::{ElementState, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+fn window_hander(_window: &mut Window, event: WindowEvent, control_flow: &mut ControlFlow) {}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    pretty_env_logger::init();
+    debug!("hello debug!");
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new().build(&event_loop)?;
+    let mut state = core::State::new(window).await?;
+    let _window_id = state.window().id();
+
+    let mut buffer = Vertex::create_buffer(&mut state, core::VERTICES);
+
+    event_loop.run(move |event, _something, control_flow| match event {
+        winit::event::Event::RedrawRequested(window_id) if window_id == _window_id => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // All other errors (Outdated, Timeout) should be resolved by the next frame
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        winit::event::Event::MainEventsCleared => {
+            state.window().request_redraw();
+        }
+        winit::event::Event::WindowEvent { window_id, event } if window_id == _window_id => {
+            match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(physical_size);
+                }
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    state.resize(*new_inner_size);
+                }
+                window_event => match state.input(&window_event) {
+                    true => (),
+                    false => (),
+                },
+            };
+        }
+        _ => (),
+    });
+}
